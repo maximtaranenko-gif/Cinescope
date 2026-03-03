1e383372839bc7c5df33f66dd805afe9c425944a
Maxim Taranenko <maximtaranenko@gmail.com>
2026-03-03 19:45:35 +0700

    New data_generator

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://restful-booker.herokuapp.com"
+BASE_URL = "https://auth.dev-cinescope.coconutqa.ru/"
 HEADERS = {
     "Content-Type": "application/json",
     "Accept": "application/json"
