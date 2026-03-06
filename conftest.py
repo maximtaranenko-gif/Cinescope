47cabe223b29c2bf3c63cdfb86fd8c183d0a60cd
Maxim Taranenko <maximtaranenko@gmail.com>
2026-03-06 13:50:28 +0700

    New custom_requester

--- conftest.py
+++ conftest.py
@@ -1,11 +1,17 @@
-import requests
-from constants import BASE_URL, HEADERS, REGISTER_ENDPOINT, LOGIN_ENDPOINT
+from faker import Faker
 import pytest
+import requests
+from constants import BASE_URL, REGISTER_ENDPOINT, LOGIN_ENDPOINT
+from custom_requester.custom_requester import CustomRequester
 from utils.data_generator import DataGenerator
+
+faker = Faker()
 
 @pytest.fixture(scope="session")
 def test_user():
-    #Генерация случайного пользователя
+    """
+    Генерация случайного пользователя для тестов.
+    """
     random_email = DataGenerator.generate_random_email()
     random_name = DataGenerator.generate_random_name()
     random_password = DataGenerator.generate_random_password()
@@ -19,26 +25,26 @@ def test_user():
     }
 
 @pytest.fixture(scope="session")
-def auth_session(test_user):
-    #Регистрация нового пользователя
-    register_url = f"{BASE_URL}{REGISTER_ENDPOINT}"
-    response = requests.post(register_url, json=test_user, headers=HEADERS)
-    assert response.status_code == 201, f"Ошибка регистрации пользователя"
+def registered_user(requester, test_user):
+    """
+    Фикстура для регистрации и получения данных зарегистрированного пользователя.
+    """
+    response = requester.send_request(
+        method="POST",
+        endpoint=REGISTER_ENDPOINT,
+        data=test_user,
+        expected_status=201
+    )
+    response_data = response.json()
+    registered_user = test_user.copy()
+    registered_user["id"] = response_data["id"]
+    return registered_user
 
-    #Логинимся для получения токена
-    login_url = f"{BASE_URL}{LOGIN_ENDPOINT}"
-    login_data = {
-        "email": test_user["email"],
-        "password": test_user["password"]
-    }
-    response = requests.post(login_url, json=login_data, headers=HEADERS)
-    assert response.status_code == 201, "Ошибка авторизации"
-
-    #Получаем токен и создаём сессию
-    token = response.json().get("accessToken")
-    assert token is not None, "Токен доступа отсуствует в ответе"
-
+@pytest.fixture(scope="session")
+def requester():
+    """
+    Фикстура для создания экземпляра CustomRequester.
+    """
     session = requests.Session()
-    session.headers.update(HEADERS)
-    session.headers.update({"Authorization": f"Bearer {token}"})
-    return session
+    return CustomRequester(session=session, base_url=BASE_URL)
+
--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -1,4 +1,3 @@
-import pytest
 import requests
 from constants import BASE_URL, HEADERS, REGISTER_ENDPOINT, LOGIN_ENDPOINT
 
@@ -10,26 +9,20 @@ class TestAuthAPI:
     LOGIN_URL = f"{BASE_URL}{LOGIN_ENDPOINT}"
 
 
-    def test_register_user(self, test_user):
-        #Отправка запроса на регистрацию
-        response = requests.post(TestAuthAPI.REGISTER_URL, json=test_user, headers=HEADERS)
-
-        #Логируем ответ для диагностики:
-        print(f"Response status: {response.status_code}")
-        print(f"Response body: {response.text}")
-
-        #Проверки
-        assert response.status_code == 201, "Ошибка регистрации пользователя"
+    def test_register_user(self, requester, test_user):
+        response = requester.send_request(
+            method="POST",
+            endpoint=REGISTER_ENDPOINT,
+            data=test_user,
+            expected_status=201
+        )
         response_data = response.json()
-        assert response_data["email"] == test_user["email"], "email не совпадает"
+        assert response_data["email"] == test_user["email"], "Email не совпадает"
         assert "id" in response_data, "ID пользователя отсутствует в ответе"
         assert "roles" in response_data, "Роли пользователя отсутствуют в ответе"
-
-        #Проверяем, что роль USER назначена по умолчанию
         assert "USER" in response_data["roles"], "Роль USER должна быть у пользователя"
 
-
-    def test_authorization_user(self, test_user):
+    def test_authorization_user(self,requester, test_user):
         #Данные для авторизации
         login_data = {
             "email":test_user["email"],
@@ -37,12 +30,11 @@ class TestAuthAPI:
         }
 
         #Отправка запроса на авторизацию
-        response = requests.post(TestAuthAPI.LOGIN_URL, json=login_data, headers=HEADERS)
-
-        #Логи авторизации
-        print(f"{response.status_code}")
-        print(f"{response.text}")
-        print("=" * 180)
+        response = requester.send_request(
+            method="POST",
+            endpoint=LOGIN_ENDPOINT,
+            data=login_data,
+            expected_status=201)
 
         #Проверки
         assert response.status_code == 201, "Авторизация не удалась"
@@ -55,77 +47,71 @@ class TestAuthAPI:
         assert "email" != "", "email пустой"
         assert "@" in response_data["user"]["email"], f"email не содержит @"
 
-
-    def test_negative_incorrect_data_authorization(self, test_user):
+    def test_negative_incorrect_data_authorization(self,requester, test_user):
         #Некорректные данные для авторизации(собственный пароль)
         login_data = {
             "email":test_user["email"],
             "password": "Geychik228"
         }
-        response = requests.post(TestAuthAPI.LOGIN_URL, json=login_data, headers=HEADERS)
+        response = requester.send_request(
+            method="POST",
+            endpoint=LOGIN_ENDPOINT,
+            data=login_data,
+            expected_status=401)
 
-        #Логи
-        print(f"Response status: {response.status_code}")
-        print(f"Response body: {response.text}")
-        print("=" * 180)
         #Проверки
         assert response.status_code == 401, f"Ожидалась ошибка 401 , получили: {response.status_code}"
 
-
-    def test_negative_incorrect_email(self, test_user):
+    def test_negative_incorrect_email(self,requester, test_user):
         #Некоректный email
         login_data = {
             "email": "maximtaranenko@gmail.com",
             "password":test_user["password"]
         }
-        response = requests.post(TestAuthAPI.LOGIN_URL, json=login_data, headers=HEADERS)
-
-        #Логи
-        print(f"Response status: {response.status_code}")
-        print(f"Response body: {response.text}")
-        print("=" * 180)
+        response = requester.send_request(
+            method="POST",
+            endpoint=LOGIN_ENDPOINT,
+            data=login_data,
+            expected_status=401)
 
         #Проверки
         assert response.status_code == 401, f"Ожидалась ошибка 401, получили {response.status_code}"
 
-
-    def test_negative_empty_body_request(self, test_user):
+    def test_negative_empty_body_request(self,requester, test_user):
         #Проверка пустым телом запроса
-        response = requests.post(TestAuthAPI.LOGIN_URL, headers=HEADERS)
-
-        #Логи
-        print(f"Response status: {response.status_code}")
-        print(f"Response body: {response.text}")
-        print("=" * 180)
-
+        response = requester.send_request(
+            method="POST",
+            endpoint=LOGIN_ENDPOINT,
+            expected_status=401)
+        #Проверки
         assert response.status_code == 401, f"Ожидалась ошибка 401, сервер вернул {response.status_code}"
 
 
-    def test_negative_empty_json(self):
+    def test_negative_empty_json(self, requester):
         #Проверка пустым json
         login_data = {
             "email":"",
             "password":""
         }
-        response = requests.post(TestAuthAPI.LOGIN_URL, json=login_data, headers=HEADERS)
+        response = requester.send_request(
+            method="POST",
+            endpoint=LOGIN_ENDPOINT,
+            data=login_data,
+            expected_status=401)
 
-        print(f"Response status: {response.status_code}")
-        print(f"Response body: {response.text}")
-        print("=" * 180)
         #Проверки
 
         assert response.status_code == 400, "Некорректный запрос"
 
 
-    def test_negative_empty_str(self):
+    def test_negative_empty_str(self, requester):
         #Проверка пустой строкой
         login_data = ""
-        response = requests.post(TestAuthAPI.LOGIN_URL, json= login_data, headers=HEADERS)
-
-        #Логи
-        print(f"Response status: {response.status_code}")
-        print(f"Response body: {response.text}")
-        print("=" * 180)
+        response = requester.send_request(
+            method="POST",
+            endpoint= LOGIN_ENDPOINT,
+            data=login_data,
+            expected_status=400)
 
         #Проверки
         assert response.status_code == 400, "Некорректный запрос"
