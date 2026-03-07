b774daa6c4ec990867770c628ddd9925f3e87bbf
Maxim Taranenko <maximtaranenko@gmail.com>
2026-03-08 01:27:05 +0700

    Change

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -1,9 +1,4 @@
-import requests
-import pytest
-
 from conftest import api_manager
-from constants import BASE_URL, REGISTER_ENDPOINT, LOGIN_ENDPOINT
-from custom_requester.custom_requester import CustomRequester
 from api.api_manager import ApiManager
 
 
@@ -47,7 +42,6 @@ class TestAuthAPI:
 
         # Проверки
         assert response.status_code == 201, "Авторизация не удалась"
-        response_data = response.json()
         assert "accessToken" in response_data, "Токен в ответе отсутствует"
         assert "user" in response_data, "Поле user отсутствует в ответе"
         # Проверка email
@@ -56,67 +50,51 @@ class TestAuthAPI:
         assert "email" != "", "email пустой"
         assert "@" in response_data["user"]["email"], f"email не содержит @"
 
-    def test_negative_incorrect_data_authorization(self,requester, test_user):
-        #Некорректные данные для авторизации(собственный пароль)
+    def test_negative_incorrect_data_authorization(self,api_manager: ApiManager, test_user):
+        """Некорректные данные для авторизации(собственный пароль)"""
         login_data = {
             "email":test_user["email"],
             "password": "Geychik228"
         }
-        response = requester.send_request(
-            method="POST",
-            endpoint=LOGIN_ENDPOINT,
-            data=login_data,
-            expected_status=401)
+        response = api_manager.auth_api.login_user(login_data)
 
         assert response.status_code == 401, f"Ожидалась ошибка 401 , получили: {response.status_code}"
 
-    def test_negative_incorrect_email(self,requester, test_user):
-        #Некоректный email
+    def test_negative_incorrect_email(self,api_manager: ApiManager, test_user):
+        """#Некоректный email"""
         login_data = {
             "email": "maximtaranenko@gmail.com",
             "password":test_user["password"]
         }
-        response = requester.send_request(
-            method="POST",
-            endpoint=LOGIN_ENDPOINT,
-            data=login_data,
-            expected_status=401)
+        response = api_manager.auth_api.login_user(login_data)
 
         assert response.status_code == 401, f"Ожидалась ошибка 401, получили {response.status_code}"
 
-    def test_negative_empty_body_request(self,requester, test_user):
-        #Проверка пустым телом запроса
-        response = requester.send_request(
-            method="POST",
-            endpoint=LOGIN_ENDPOINT,
-            expected_status=401)
+    def test_negative_empty_body_request(self,api_manager:ApiManager, test_user):
+        """Проверка пустым телом запроса"""
+        login_data = {
+
+        }
+        response = api_manager.auth_api.login_user(login_data)
 
         assert response.status_code == 401, f"Ожидалась ошибка 401, сервер вернул {response.status_code}"
 
 
-    def test_negative_empty_json(self, requester):
-        #Проверка пустым json
+    def test_negative_empty_json(self, api_manager: ApiManager):
+        """Проверка пустым json"""
         login_data = {
             "email":"",
             "password":""
         }
-        response = requester.send_request(
-            method="POST",
-            endpoint=LOGIN_ENDPOINT,
-            data=login_data,
-            expected_status=401)
+        response = api_manager.auth_api.login_user(login_data)
 
         assert response.status_code == 400, "Некорректный запрос"
 
 
-    def test_negative_empty_str(self, requester):
-        #Проверка пустой строкой
+    def test_negative_empty_str(self, api_manager: ApiManager):
+        """Проверка пустой строкой"""
         login_data = ""
-        response = requester.send_request(
-            method="POST",
-            endpoint= LOGIN_ENDPOINT,
-            data=login_data,
-            expected_status=400)
+        response = api_manager.auth_api.login_user(login_data)
 
         assert response.status_code == 400, "Некорректный запрос"
 
