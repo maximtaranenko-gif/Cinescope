29b27044d77b85fc342ca7c2698fe83a13844c3b
Maxim Taranenko <maximtaranenko@gmail.com>
2026-03-18 19:50:08 +0700

    v 2.1.1

--- tests/movies/test_delete_movie.py
+++ tests/movies/test_delete_movie.py
@@ -1,17 +1,15 @@
 from constants import USER_CREDS
-from conftest import movie_data, movie_data_incorrect
+from conftest import movie_data
 
 class TestMovieAPI:
     def test_delete_movie(self, api_manager, movie_data):
         """Тест на удаление конкретного фильма"""
         api_manager.auth_api.authenticate(USER_CREDS)
-        """Создаем фильм и вытягиваем ID из ответа """
         create_response = api_manager.movie_api.create_movie(movie_data, expected_status=201)
-        created_movie = create_response.json()
-        movie_id = created_movie["id"]
-        """Удаляем фильм по полученному ID"""
+        movie_created = create_response.json()
+        movie_id = movie_created["id"]
         delete_response = api_manager.movie_api.delete_movie(movie_id, expected_status=200)
-        assert delete_response.status_code == 200, "Фильм не был найден"
+        assert delete_response.status_code == 200
 
         """Вызываем GET-запрос для того чтобы убедиться, что фильм удален"""
         get_response = api_manager.movie_api.get_movie(movie_id, expected_status=404)
